24838b8dd15bc9c7e2af09f9c1fc6b800fa7878f
Erik-Westerstrahle <erikwesterstrahle@gmail.com>
2024-02-02 13:04:32 +0200

    began adding system for player character to level up

--- src/space_mining_sim/playerStats.java
+++ src/space_mining_sim/playerStats.java
@@ -4,12 +4,24 @@ public class playerStats {
 	
 	int reputationPlayer = 	100;
 	
+	int geologistSkillPlayer = 0;
+	int astrogatorSkillPlayer = 0;
+	int engineeringSkillPlayer = 0;
+	
+	
+	int geologistExperiencePlayer = 0;
+	int astrogatorExperiencePlayer = 0;
+	int engineeringExperiencePlayer = 0;
 	
 	public void increasePlayersReputation(int amountIncreasePlayerReputation)
 	{
 		reputationPlayer+=amountIncreasePlayerReputation;
 	}
 	
+	public void increaseGeologistExperiencePlayer ()
+	{
+		
+	}
 	
 	
 	
