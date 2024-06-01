f627d40fcb0a5f1158db52a88e8841a3a0407b7a
Erik-Westerstrahle <erikwesterstrahle@gmail.com>
2024-06-01 14:05:59 +0300

    added beginning for option to turn of sound

--- src/space_mining_sim/optionsSpaceMiningSim.java
+++ src/space_mining_sim/optionsSpaceMiningSim.java
@@ -1,5 +1,36 @@
 package space_mining_sim;
 
+import java.util.Scanner;
+
+
 public class optionsSpaceMiningSim {
+	
+	boolean soundBoolean = true;
+	
+	
+	public void userInput(String input)
+	{
+	 switch (input) {
+	 case "1":
+		 setSoundBoolean(false);
+		  break;
+	 }
+	}
+	
+	
+	
+	
+	
+	
+	
+	public boolean getSoundBoolean()
+	{
+		return soundBoolean;
+	}
+	
+    public void setSoundBoolean(boolean newSoundBoolean) {
+    	// TODO Auto-generated method stub
+    	this.soundBoolean = newSoundBoolean;
+    }
 
 }
